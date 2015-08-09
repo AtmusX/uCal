968d9c8f901f1e75c77b62c4c6f5af09fa2c49aa
Richard Hansen <rrhansen_19@yahoo.com>
2015-08-09 12:11:49 -0700

    v8 hasn't enabled find or findIndex, also fixed incorrect yield invocation

--- controllers/entity.js
+++ controllers/entity.js
@@ -11,14 +11,14 @@ module.exports.home = function * home(next) {
 
 module.exports.all = function * all(next) {
   if ('GET' != this.method) return yield next;
-  this.body = yield entities.find({});
+  this.body = entities;
 };
 
 module.exports.fetch = function * fetch(id, next) {
   if ('GET' != this.method) return yield next;
   // Quick hack.
   if(id === '' + parseInt(id, 10)) {
-    var entity = yield entities.find(function (element) {
+    let entity = yield entities.filter(function (element) {
       return id === element.id;
     });
     if (entity.length === 0) {
@@ -30,20 +30,25 @@ module.exports.fetch = function * fetch(id, next) {
 
 module.exports.add = function * add(data, next) {
   if ('POST' != this.method) return yield next;
-  var entity = data; // sanitize your input!
-  var inserted = yield entities.push(entity);
+  let entity = data; // sanitize your input!
+  let inserted = entities.push(entity);
   this.body = 'Done!';
 };
 
 module.exports.modify = function * modify(id, next) {
   if ('PUT' != this.method) return yield next;
 
-  var data = yield parse(this, {
+  let data = yield parse(this, {
     limit: '1kb'
   });
 
-  var entityIndex = yield entities.findIndex(function (element) {
-    return id === element.id;
+  let entityIndex = -1;
+  let entity = yield entities.filter(function (element, index) {
+    let check = id === element.id;
+    if (check) {
+      entityIndex = index;
+    }
+    return check;
   });
 
   if (entityIndex < 0) {
@@ -58,8 +63,13 @@ module.exports.modify = function * modify(id, next) {
 module.exports.remove = function * remove(id, next) {
   if ('DELETE' != this.method) return yield next;
 
-  var entityIndex = yield entities.findIndex(function (element) {
-    return id === element.id;
+  let entityIndex = -1;
+  let entity = yield entities.filter(function (element, index) {
+    var check = id === element.id;
+    if (check) {
+      entityIndex = index;
+    }
+    return check;
   });
 
   if (entityIndex < 0) {
